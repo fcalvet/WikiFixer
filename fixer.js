5c0ddbc844ae1048fdc8b7dd0547ea5595be1013
fcalvet <38648518+fcalvet@users.noreply.github.com>
2020-10-01 21:25:53 +0200

    Update for header toolbar
    Headbar toolbar got centered and limited in width, fixing.

--- fixer.js
+++ fixer.js
@@ -1,8 +1,10 @@
 /*
-Just draw a border round the document.body.
+parking lot:
+.skin-vector-max-width .mw-body {border: 1px solid #a7d7f9; border-right-width: 0; padding: 1.25em 1.5em 1.5em 1.5em; }
 */
 var editCSS = document.createElement('style')
 editCSS.innerHTML = `.skin-vector-max-width .mw-page-container { max-width: unset; margin-left: unset; margin-right: unset; }
 .skin-vector-max-width .mw-content-container { max-width: unset; margin-left: 150px;}
-.skin-vector-max-width .mw-workspace-container { max-width: unset; }`;
+.skin-vector-max-width .mw-workspace-container { max-width: unset; }
+.skin-vector-max-width.skin-vector-search-header .mw-article-toolbar-container { max-width: unset; margin-left: 145px; }`;
 document.body.appendChild(editCSS);
