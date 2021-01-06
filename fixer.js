cfe1fc05d3844893671a811fa98ac7272d0cf8bf
fcalvet <38648518+fcalvet@users.noreply.github.com>
2021-01-06 18:23:24 +0100

    removing obsolete CSS prefix

--- fixer.js
+++ fixer.js
@@ -3,8 +3,8 @@ parking lot:
 .skin-vector-max-width .mw-body {border: 1px solid #a7d7f9; border-right-width: 0; padding: 1.25em 1.5em 1.5em 1.5em; }
 */
 var editCSS = document.createElement('style')
-editCSS.innerHTML = `.skin-vector-max-width .mw-page-container { max-width: unset; margin-left: unset; margin-right: unset; }
-.skin-vector-max-width .mw-content-container { max-width: unset; margin-left: 150px;}
-.skin-vector-max-width .mw-workspace-container { max-width: unset; }
-.skin-vector-max-width.skin-vector-search-header .mw-article-toolbar-container { max-width: unset; margin-left: 145px; }`;
+editCSS.innerHTML = `.mw-page-container { max-width: unset; margin-left: unset; margin-right: unset; }
+.mw-content-container { max-width: unset; margin-left: 150px;}
+.mw-workspace-container { max-width: unset; }
+.mw-article-toolbar-container { max-width: unset; margin-left: 145px; }`;
 document.body.appendChild(editCSS);
